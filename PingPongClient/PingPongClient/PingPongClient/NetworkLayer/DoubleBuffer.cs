a97f338675c4d99524168c8f5a163298b22948f5
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-06 19:17:17 +0200

    First Commit
    Basic structure and NetworkLibrary

---
+++ PingPongClient/PingPongClient/PingPongClient/NetworkLayer/DoubleBuffer.cs
@@ -0,0 +1,38 @@
+﻿using NetworkLibrary.DataStructs;
+
+namespace PingPongClient.NetworkLayer
+{
+    class DoubleBuffer<T>
+    {
+        T Buffer1 { get; set; }
+        T Buffer2 { get; set; }
+
+        bool Switch { get; set; }
+
+        public DoubleBuffer()
+        {
+            Switch = false;
+
+            Buffer1 = new T;
+            Buffer2 = new T();
+        }
+
+        public T Read()
+        {
+            if (Switch)
+                return Buffer1;
+            else
+                return Buffer2;
+        }
+
+        public void Write(T buffer)
+        {
+            if (!Switch)
+                Buffer1 = buffer;
+            else
+                Buffer2 = buffer;
+
+            Switch = !Switch;
+        }
+    }
+}
