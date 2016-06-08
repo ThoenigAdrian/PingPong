a13ab9abd7e42d21a778be1acc6ffa431db8de12
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-08 21:47:39 +0200

    ServerData Adapter

--- NetworkLibrary/PackageAdapters/ServerUDPAdapter.cs
+++ NetworkLibrary/PackageAdapters/ServerUDPAdapter.cs
@@ -7,12 +7,31 @@ namespace NetworkLibrary.PackageAdapters
     {
         public override PackageInterface ByteToPackage(byte[] data)
         {
-            throw new NotImplementedException();
+            ServerDataPackage package = new ServerDataPackage();
+            byte[] buffer = new byte[4];
+
+            Array.Copy(data, 0, buffer, 0, buffer.Length);
+            package.BallPosX = Convert.ToInt32(buffer);
+
+            Array.Copy(data, 4, buffer, 0, buffer.Length);
+            package.BallPosY = Convert.ToInt32(buffer);
+
+            return package;
         }
 
         public override byte[] PackageToByte(PackageInterface package)
         {
-            throw new NotImplementedException();
+            ServerDataPackage serverPackage = package as ServerDataPackage;
+ 
+            byte[] data = new byte[8];
+
+            byte[] buffer = BitConverter.GetBytes(serverPackage.BallPosX);
+            Array.Copy(buffer, 0, data, 0, buffer.Length);
+
+            buffer = BitConverter.GetBytes(serverPackage.BallPosY);
+            Array.Copy(buffer, 0, data, 0, buffer.Length);
+
+            return data;
         }
     }
 }
