99e135cf8830276fa6103bd1a8152111f886865a
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-08 20:22:49 +0200

    Player input structure
    - basic structure

--- PingPongClient/PingPongClient/PingPongClient/InputLayer/InputInterface.cs
+++ PingPongClient/PingPongClient/PingPongClient/InputLayer/InputInterface.cs
@@ -1,8 +1,4 @@
 ﻿using GameLogicLibrary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace PingPongClient.InputLayer
 {
---
+++ PingPongClient/PingPongClient/PingPongClient/InputLayer/InputTranslators/KeyboardControllTranslation.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Input;
+using GameLogicLibrary;
+
+namespace PingPongClient.InputLayer.InputTranslators
+{
+    abstract class KeyboardControllTranslation
+    {
+        public abstract Keys GetKeyForAction(ClientControls action);
+    }
+}
