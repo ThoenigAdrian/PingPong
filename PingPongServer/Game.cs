98d4a54b9b8d848321cf6e825ead6c0b79f9bb30
Adrian <thoenig.adrian@hotmail.com>
2016-06-09 21:35:31 +0200

    Server Development started

---
+++ PingPongServer/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using GameLogicLibrary;
+using System.Collections.Generic;
+using NetworkLibrary.DataPackages;
+using NetworkLibrary.NetworkImplementations.ConnectionImplementations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer
+{
+    class Game
+    {
+        List<Player> Players = new List<Player>();
+        
+        public Game(ServerNetwork Network)
+        {
+            
+        }
+        public int StartGame()
+        {
+            ServerDataPackage ServerPackage = new ServerDataPackage();
+            CalculateFrame();
+            Network.BroadcastUDP();
+            return 0;
+        }
+        private class Player
+        {
+            public ClientMovement PlayerMovement {get; set;}
+            public TCPConnection ClientConnection;
+            private Player(TCPConnection ClientConnection)
+            {
+                this.ClientConnection = ClientConnection;
+            }
+            private void PackageReceivedEvent(object sender, EventArgs e)
+            {
+                PackageInterface getPackageType = (PackageInterface)sender;
+                PlayerMovement = (ClientMovement)sender;
+            }
+        }
+    }
+}
