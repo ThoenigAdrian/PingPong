73521f7dfe9299d942e6a05cbec97cbd9f9295f0
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-09 16:35:23 +0200

    files got deleted
    i dont fucking know why

---
+++ NetworkLibrary/DataStructs/ClientControlPackage.cs
@@ -0,0 +1,14 @@
+﻿using GameLogicLibrary;
+
+namespace NetworkLibrary.DataStructs
+{
+    public class ClientControlPackage : PackageInterface
+    {
+        public override PackageType PackageType
+        {
+            get { return PackageType.ClientControl; }
+        }
+
+        public ClientControls ControlInput;
+    }
+}
