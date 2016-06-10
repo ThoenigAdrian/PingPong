51a24bfcc6aa295e2d124f0f7f05af7a4f13a34c
Adrian <thoenig.adrian@hotmail.com>
2016-06-10 11:55:55 +0200

    Resolve merge conflict2

---
+++ NetworkLibrary/DataPackages/ServerSourcePackages/ServerGameControlPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameLogicLibrary;
+
+namespace NetworkLibrary.DataPackages.ServerSourcePackages
+{
+    class ServerGameControlPackage
+    {
+        public ServerControls Command;
+    }
+}
