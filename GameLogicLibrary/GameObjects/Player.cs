82d616cd6286a12778f4259d535362141a55a61a
Adrian <thoenig.adrian@hotmail.com>
2016-06-14 22:45:03 +0200

    Remove unneccessary usings

--- GameLogicLibrary/GameObjects/Player.cs
+++ GameLogicLibrary/GameObjects/Player.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GameLogicLibrary.GameObjects
+﻿namespace GameLogicLibrary.GameObjects
 {
     public class Player
     {
