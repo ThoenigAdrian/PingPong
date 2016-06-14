48631ae92e545ef36924604d122dd60b6e7c0392
Adrian <thoenig.adrian@hotmail.com>
2016-06-14 22:44:33 +0200

    Add Player Class

---
+++ GameLogicLibrary/GameObjects/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogicLibrary.GameObjects
+{
+    public class Player
+    {
+        public PlayerBar PlayerBar { get; set; }
+        public ClientMovement PlayerMovement { get; set; }
+
+        private Teams Team;
+        private int PlayerID { get; set; }
+        
+        public Player(int PlayerID, Teams Team, float PositionX)
+        {
+            this.PlayerID = PlayerID;
+            this.Team = Team;
+            PlayerBar = new PlayerBar(PositionX);
+        }
+     }
+}
