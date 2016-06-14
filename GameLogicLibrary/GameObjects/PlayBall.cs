062bfef0d783b9962a93c1fa93e839d16ccef8e4
Adrian <thoenig.adrian@hotmail.com>
2016-06-15 00:25:02 +0200

    Renaming

--- GameLogicLibrary/GameObjects/PlayBall.cs
+++ GameLogicLibrary/GameObjects/PlayBall.cs
@@ -2,15 +2,15 @@
 {
     public class PlayBall
     {
-        public float PosX { get; set; }
-        public float PosY { get; set; }
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
 
         public int Radius { get; set; }
 
         public PlayBall()
         {
-            PosX = GameInitializers.BALL_POSX;
-            PosY = GameInitializers.BALL_POSY;
+            PositionX = GameInitializers.BALL_POSX;
+            PositionY = GameInitializers.BALL_POSY;
 
             Radius = GameInitializers.BALL_RADIUS;
         }
