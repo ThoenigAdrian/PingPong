6c795dde48fad0b32041ed2c98046452a52cee9f
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-14 14:55:19 +0200

    many more features to test many more bugs

--- ConnectionTesting/ConnectionTesting/Server.cs
+++ ConnectionTesting/ConnectionTesting/Server.cs
@@ -1,22 +1,34 @@
 ﻿using NetworkLibrary.DataPackages;
 using NetworkLibrary.DataPackages.ServerSourcePackages;
+using NetworkLibrary.NetworkImplementations;
 using NetworkLibrary.NetworkImplementations.ConnectionImplementations;
 using NetworkLibrary.Utility;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
 namespace ConnectionTesting
 {
+    enum SendMode
+    {
+        TCP,
+        UDP
+    }
+
     class Server
     {
         ServerNetwork m_network;
+        SessionIDPool m_sessionPool;
 
         Listening Listen;
+        Sending Sender;
+
+        LogWriter m_logger = new LogWriterConsole();
 
         bool m_stopServer = false;
-        bool m_spammingActive = false;
+
 
         public SafeStack<string> m_commandStack;
 
@@ -25,28 +37,31 @@ namespace ConnectionTesting
 
         public Server()
         {
+            m_sessionPool = new SessionIDPool();
             m_commandStack = new SafeStack<string>();
             Listen = new Listening(4200);
+            Sender = new Sending();
         }
 
         public void StartServer()
         {
-            Console.Out.WriteLine("Initializing server...");
+            m_logger.Log("Initializing server...");
 
             if (!Listen.InitialiseListening())
             {
-                Console.Out.WriteLine("Initializing error!\nServer shut down.");
+                m_logger.Log("Initializing error!\nServer shut down.");
                 if (ServerInitError != null)
                     ServerInitError.Invoke();
 
                 return;
             }
 
-
             m_network = new ServerNetwork(new UDPConnection(new IPEndPoint(IPAddress.Any, 4200)));
             m_network.SessionDied += ClientDisconnectHandler;
 
-            Console.Out.WriteLine("Server started.");
+            Sender.m_network = m_network;
+
+            m_logger.Log("Server started.");
 
             ServerCylce();
         }
@@ -60,8 +75,8 @@ namespace ConnectionTesting
                 ReceiveData();
                 ExecuteCommand();
 
-                if (m_spammingActive)
-                    Broadcast();
+                if (Sender.m_spammingActive)
+                    Sender.Broadcast();
             }
         }
 
@@ -70,24 +85,35 @@ namespace ConnectionTesting
             Socket acceptedSocket;
             while ((acceptedSocket = Listen.m_socketQueue.Read()) != null)
             {
-                NetworkConnection clientConnection = new NetworkConnection(new TCPConnection(acceptedSocket));
+                int sessionID = m_sessionPool.GetSessionID;
+                NetworkConnection clientConnection = new NetworkConnection(new TCPConnection(acceptedSocket), sessionID);
+                ServerSessionResponse response = new ServerSessionResponse();
+                response.ClientSessionID = sessionID;
+                clientConnection.SendTCP(response);
                 m_network.AddClientConnection(clientConnection);
 
-                Console.Out.WriteLine("Client connected.");
+                m_logger.Log("Client connected.");
             }
         }
 
         private void ReceiveData()
         {
-            for (int i = 0; i < m_network.ClientCount; i++)
+            Dictionary<int, PackageInterface[]> tcpPackages = m_network.GetAllSessionDataTCP();
+            if(tcpPackages != null)
             {
-                if(m_network.ReceivePackageUDP(i) != null)
-                    Console.Out.WriteLine("Received UDP package from session " + i + ".");
+                foreach (KeyValuePair<int, PackageInterface[]> entry in tcpPackages)
+                {
+                    m_logger.Log("Received " + entry.Value.Length + " TCP packages from session " + entry.Key + ".");
+                }
+            }
 
-                PackageInterface[] tcpPackages = m_network.ReceivePackageTCP(i);
-
-                if(tcpPackages.Length > 0)
-                    Console.Out.WriteLine("Received " + tcpPackages.Length + " TCP packages from session " + i + ".");
+            Dictionary<int, PackageInterface> udpPackages = m_network.GetAllSessionDataUDP();
+            if (udpPackages != null)
+            {
+                foreach (KeyValuePair<int, PackageInterface> entry in udpPackages)
+                {
+                    m_logger.Log("Received UDP package from session " + entry.Key + ".");
+                }
             }
         }
 
@@ -96,31 +122,41 @@ namespace ConnectionTesting
             string cmd;
             while ((cmd = m_commandStack.Read()) != default(string))
             {
-                switch(cmd)
+                if (cmd.Contains("interval"))
                 {
-                    case "send":
-                        Broadcast();
-                        break;
-                    case "spam":
-                        m_spammingActive = !m_spammingActive;
-                        break;
+                    string[] split = cmd.Split(' ');
+                    if (split.Length > 1)
+                    {
+                        try { Sender.m_sendInterval = Convert.ToInt32(split[1]); }
+                        catch { Sender.m_sendInterval = 0; }
+                    }
+                }
+                else
+                {
+                    switch (cmd)
+                    {
+                        case "mode tcp":
+                            Sender.m_sendMode = SendMode.TCP;
+                            m_logger.Log("Now sending in TCP mode.");
+                            break;
+                        case "mode udp":
+                            Sender.m_sendMode = SendMode.UDP;
+                            m_logger.Log("Now sending in UDP mode.");
+                            break;
+                        case "send":
+                            Sender.Broadcast();
+                            break;
+                        case "spam":
+                            Sender.m_spammingActive = !Sender.m_spammingActive;
+                            break;
+                    }
                 }
             }
         }
 
-        private void Broadcast()
-        {
-            ServerDataPackage package = new ServerDataPackage();
 
-            package.Ball.PositionX = 100;
-            package.Ball.PositionY = 200;
 
-            m_network.SendPositionData(package);
-
-            Console.Out.WriteLine("Sent position data.");
-        }
-
-        private void ClientDisconnectHandler(int sessionID)
+        private void ClientDisconnectHandler(NetworkInterface sender, int sessionID)
         {
             Console.Out.WriteLine("Client disconnected.");
         }
@@ -192,6 +228,77 @@ namespace ConnectionTesting
         {
             m_abortAccepting = true;
             m_listeningSocket.Close();
+        }
+    }
+
+    class Sending
+    {
+        public bool m_spammingActive = false;
+        public SendMode m_sendMode = SendMode.UDP;
+        public int m_sendInterval = 0;
+        DateTime m_lastSendStamp;
+        public ServerNetwork m_network;
+
+        Random m_random = new Random();
+        float BallX = 0;
+        float BallY = 0;
+        float dirX = 0.1F;
+        float dirY = 0.1F;
+
+        public Sending()
+        {
+            m_lastSendStamp = DateTime.Now;
+        }
+
+        public void Broadcast()
+        {
+            if (m_network == null)
+                return;
+
+            DateTime currentTime = DateTime.Now;
+
+            if (currentTime.Ticks - (m_sendInterval * 10000) < m_lastSendStamp.Ticks)
+                return;
+
+            ServerDataPackage package = new ServerDataPackage();
+
+            BallX += dirX;
+            if (BallX <= 0 || BallX > GameLogicLibrary.GameInitializers.BORDER_WIDTH)
+                dirX *= -1;
+
+            BallY += dirY;
+            if (BallY <= 0 || BallY > GameLogicLibrary.GameInitializers.BORDER_HEIGHT)
+                dirY *= -1;
+
+            package.Ball.PositionX = BallX;
+            package.Ball.PositionY = BallY;
+
+            switch (m_sendMode)
+            {
+                case SendMode.UDP:
+                    m_network.SendPositionDataUDP(package);
+                    break;
+
+                case SendMode.TCP:
+                    m_network.SendPositionDataTCP(package);
+                    break;
+            }
+
+            m_lastSendStamp = currentTime;
+            Console.Out.WriteLine("Sent position data.");
+        }
+    }
+
+    class SessionIDPool
+    {
+        int m_nextSessionID = 0;
+
+        public int GetSessionID
+        {
+            get
+            {
+                return m_nextSessionID++;
+            }
         }
     }
 }
--- ConnectionTesting/ConnectionTesting/ServerNetwork.cs
+++ ConnectionTesting/ConnectionTesting/ServerNetwork.cs
@@ -2,6 +2,7 @@
 using NetworkLibrary.DataPackages.ServerSourcePackages;
 using NetworkLibrary.NetworkImplementations;
 using NetworkLibrary.NetworkImplementations.ConnectionImplementations;
+using System.Collections.Generic;
 
 namespace ConnectionTesting
 {
@@ -12,9 +13,48 @@ namespace ConnectionTesting
             
         }
 
-        public void SendPositionData(ServerDataPackage package)
+        public void SendPositionDataUDP(ServerDataPackage package)
         {
             BroadCastUDP(package);
+        }
+
+        public void SendPositionDataTCP(ServerDataPackage package)
+        {
+            BroadCastTCP(package);
+        }
+
+        public Dictionary<int, PackageInterface[]> GetAllSessionDataTCP()
+        {
+            Dictionary<int, PackageInterface[]> packages = new Dictionary<int, PackageInterface[]>();
+
+            foreach (int session in GetSessionIDs)
+            {
+                PackageInterface[] sessionPackages = GetAllDataTCP(session);
+                if(sessionPackages != null)
+                    packages.Add(session, sessionPackages);
+            }
+
+            if (packages.Count > 0)
+                return packages;
+
+            return null;
+        }
+
+        public Dictionary<int, PackageInterface> GetAllSessionDataUDP()
+        {
+            Dictionary<int, PackageInterface> packages = new Dictionary<int, PackageInterface>();
+
+            foreach (int session in GetSessionIDs)
+            {
+                PackageInterface sessionPackage = GetDataUDP(session);
+                if(sessionPackage != null)
+                    packages.Add(session, sessionPackage);
+            }
+
+            if (packages.Count > 0)
+                return packages;
+
+            return null;
         }
 
         public PackageInterface[] ReceivePackageTCP(int session)
