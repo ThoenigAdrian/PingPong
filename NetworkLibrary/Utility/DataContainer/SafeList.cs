bca2c1774d2916e8d61e0d5a228dacc19c5e4918
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-16 17:59:20 +0200

    added and tested features of safelist
    - "safe" to say its working pretty good
    - not being able to loop around it with "foreach" is desired as you should use SafeList.Entries for looping (makes a copy of the list)

--- NetworkLibrary/Utility/DataContainer/SafeList.cs
+++ NetworkLibrary/Utility/DataContainer/SafeList.cs
@@ -1,23 +1,71 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 
-namespace NetworkLibrary.Utility.DataContainer
+namespace NetworkLibrary.Utility
 {
-    class SafeList<T> : IDisposable
+    public class SafeList<T> : IDisposable
     {
         List<T> m_internalList;
         Semaphore m_listLock;
 
-        public SafeList() : this(new List<T>())
+        public SafeList() : this (new List<T>())
         {
         }
+
         public SafeList(List<T> list)
         {
-            m_listLock = new Semaphore(1,1);
+            m_listLock = new Semaphore(1, 1);
             m_internalList = list;
+        }
+
+
+        public List<T> Entries
+        {
+            get
+            {
+                m_listLock.WaitOne();
+                try { return new List<T>(m_internalList); }
+                finally { m_listLock.Release(); }
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                m_listLock.WaitOne();
+                try { return m_internalList[index]; }
+                finally { m_listLock.Release(); }
+            }
+            set
+            {
+                m_listLock.WaitOne();
+                try { m_internalList[index] = value; }
+                finally { m_listLock.Release(); }
+            }
+        }
+
+        public T GetEntry(int index)
+        {
+            m_listLock.WaitOne();
+            try { return m_internalList[index]; }
+            finally { m_listLock.Release(); }
+        }
+
+        public void SetEntry(int index, T entry)
+        {
+            m_listLock.WaitOne();
+            try { m_internalList[index] = entry; }
+            finally { m_listLock.Release(); }
+        }
+
+        public void Insert(int index, T entry)
+        {
+            m_listLock.WaitOne();
+            try { m_internalList.Insert(index, entry); }
+            finally { m_listLock.Release(); }
         }
 
         public void Add(T entry)
@@ -27,25 +75,40 @@ namespace NetworkLibrary.Utility.DataContainer
             finally { m_listLock.Release(); }
         }
 
-        public void Remove(T entry)
+        public bool Remove(T entry)
         {
             m_listLock.WaitOne();
-            try { m_internalList.Remove(entry); }
+            try { return m_internalList.Remove(entry); }
             finally { m_listLock.Release(); }
         }
 
-        public List<T> GetList()
+        public void RemoveAt(int index)
         {
             m_listLock.WaitOne();
-            try { return new List<T>(m_internalList); }
+            try { m_internalList.RemoveAt(index); }
             finally { m_listLock.Release(); }
         }
 
-        public void Dispose()
+        public void Clear()
         {
             m_listLock.WaitOne();
             try { m_internalList.Clear(); }
             finally { m_listLock.Release(); }
+        }
+
+        public int Count
+        {
+            get
+            {
+                m_listLock.WaitOne();
+                try { return m_internalList.Count; }
+                finally { m_listLock.Release(); }
+            }
+        }
+
+        public void Dispose()
+        {
+            Clear();
             m_listLock.Dispose();
         }
     }
