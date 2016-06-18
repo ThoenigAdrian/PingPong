56966d033af595f0891dbe01e4ef0fbf45b02a10
CaptainOachkatzl <dave.hofer@hotmail.com>
2016-06-18 13:29:54 +0200

    Implemented Subcontrol server response requests

---
+++ PingPongClient/PingPongClient/PingPongClient/NetworkLayer/SubControlResponseRequest.cs
@@ -0,0 +1,15 @@
+﻿using NetworkLibrary.DataPackages;
+using NetworkLibrary.Utility;
+
+namespace PingPongClient.NetworkLayer
+{
+    public class SubControlResponseRequest : ResponseRequest
+    {
+        public GameMode Issuer { get; private set; }
+
+        public SubControlResponseRequest(GameMode issuer, PackageType responseType, long timeOutMilliseconds) : base(responseType, timeOutMilliseconds)
+        {
+            Issuer = issuer;
+        }
+    }
+}
