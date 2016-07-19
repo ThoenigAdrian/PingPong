981fd89b1c539e7d2778276502be674eb6adadcc
Adrian <thoenig.adrian@hotmail.com>
2016-07-19 23:16:38 +0200

    Player can now influence the direction of the moving ball, added option for configuring if friendly fire should be allowed

--- GameLogicLibrary/GameObjects/PlayBall.cs
+++ GameLogicLibrary/GameObjects/PlayBall.cs
@@ -1,4 +1,6 @@
-﻿namespace GameLogicLibrary.GameObjects
+﻿using System;
+
+namespace GameLogicLibrary.GameObjects
 {
     public class RawBall
     {
@@ -11,6 +13,8 @@
     public class Ball : RawBall
     {
         public int Radius { get; set; }
+        public int LastTouchedTeam;
+        private float internal_speed;
 
         public Ball()
         {
@@ -18,7 +22,24 @@
             PositionY = GameInitializers.BALL_POSY;
             DirectionX = GameInitializers.BALL_DIRX;
             DirectionY = GameInitializers.BALL_DIRY;
+            Speed = 1F;
+            LastTouchedTeam = -1; // = No Team        + refactor Teams stuff e.g Team Blue , Team Red or something different but it doesn't make sense to have a enum like this (Team1,Team2,Team3)
             Radius = GameInitializers.BALL_RADIUS;
+        }
+
+        public float Speed
+        {
+            get
+            {
+                return (float) Math.Sqrt(DirectionX * DirectionX + DirectionY * DirectionY);
+            }
+            set
+            {
+                internal_speed = value;
+                DirectionX = DirectionX * internal_speed;
+                DirectionY = DirectionY * internal_speed;
+                
+            }
         }
     }
 }
