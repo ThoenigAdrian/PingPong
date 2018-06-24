6e11b4b744397e81db61d0dd8b5f38718e728772
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-06-24 08:51:13 +0200

    setup to use XSLibrary

--- NetworkLibrary/Utility/DataContainer/SingleBuffer.cs
+++ NetworkLibrary/Utility/DataContainer/SingleBuffer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-
-namespace NetworkLibrary.Utility
+﻿namespace NetworkLibrary.Utility
 {
     public class SingleBuffer<T> : DataContainer<T>
     {
