2950f4cc3d1da8c27bdaa21636621fa3e9442e1c
Adrian <thoenig.adrian@hotmail.com>
2018-07-01 23:32:50 +0200

    Remove unneeded using

--- PingPongServer/Matchmaking/Matchmaking.cs
+++ PingPongServer/Matchmaking/Matchmaking.cs
@@ -1,5 +1,4 @@
-﻿using NetworkLibrary.NetworkImplementations.ConnectionImplementations;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace PingPongServer.Matchmaking
 {
