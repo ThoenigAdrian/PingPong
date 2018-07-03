fe1adfe5e952de5cf6b9547b81fadf4e7f3ab08f
Adrian Thoenig <thoenig.adrian@hotmail.com>
2018-07-03 22:42:40 +0200

    Move server configuraiton related stuff into own Class

---
+++ PingPongServer/ServerConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using NetworkLibrary;
+using NetworkLibrary.Utility;
+using Newtonsoft.Json.Linq;
+
+
+namespace PingPongServer
+{
+    public class ServerConfiguration
+    {
+        public int MaximumNumberOfIncomingConnections;
+        public int ServerPort;
+        private readonly string ConfigurationFile = "server_config.json";
+        private LogWriterConsole Logger { get; set; } = new LogWriterConsole();
+        
+        
+
+        public ServerConfiguration()
+        {
+            ServerPort = NetworkConstants.SERVER_PORT;
+        }
+
+        private void ApplyDefaultConfiguration()
+        {
+            MaximumNumberOfIncomingConnections = 1000;
+        }
+
+        public void ReadConfigurationFromConfigurationFile()
+        {
+            string serverConfig = "";
+            try
+            {
+                using (StreamReader serverConfigReadStream = new StreamReader(File.Open(ConfigurationFile, FileMode.Open, FileAccess.ReadWrite, FileShare.Read)))
+                {
+                    serverConfig = serverConfigReadStream.ReadToEnd();
+                    Logger.Log("Configuration file " + ConfigurationFile + " was read.");
+                }
+                JObject parsedServerConfiguration = JObject.Parse(serverConfig);
+                Logger.Log("Configuration file " + ConfigurationFile + " was parsed by Json Parser.");
+                try
+                {
+                    MaximumNumberOfIncomingConnections = (int)parsedServerConfiguration["maximumNumberOfIncomingConnections"];
+                    Logger.Log("Applied all configurations from configuration file successfully");
+                }
+
+                catch (Exception exception)
+                {
+                    Logger.Log("Couldn't read maximumNumberOfConnections from configuration file , details : ");
+                    Logger.Log(exception.Message);
+                }
+
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Log("No configuration file for server found using default configuration");
+            }
+            catch (Newtonsoft.Json.JsonReaderException exception)
+            {
+                Logger.Log("Server configuration file is invalid, Additional Information : ");
+                Logger.Log(exception.Message);
+                Logger.Log("[Warning] Default Configuraiton will be used instead !\n");
+            }
+
+        }
+    }
+}
