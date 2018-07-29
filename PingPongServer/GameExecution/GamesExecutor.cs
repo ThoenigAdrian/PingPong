2484bd3e1d07079c7d5408ba64ebc800662b9826
Adrian Thoenig <thoenig.adrian@hotmail.com>
2018-07-29 16:57:26 +0200

    Change time steps to tenths of milliseconds since measuring in millseconds can have severe rounding errors with high frame rates, Report Frame Rate for Performance Evaluation, Add Methods for rejoining clients, adding observere, and querying number of active players

--- PingPongServer/GameExecution/GamesExecutor.cs
+++ PingPongServer/GameExecution/GamesExecutor.cs
@@ -1,5 +1,8 @@
-﻿using NetworkLibrary.Utility;
+﻿using GameLogicLibrary;
+using NetworkLibrary.NetworkImplementations.ConnectionImplementations;
+using NetworkLibrary.Utility;
 using PingPongServer.ServerGame;
+using System;
 using System.Diagnostics;
 using XSLibrary.ThreadSafety.Containers;
 using XSLibrary.ThreadSafety.Locks;
@@ -11,8 +14,11 @@ namespace PingPongServer.GameExecution
         LogWriterConsole Logger = new LogWriterConsole();
         SafeList<Game> Games = new SafeList<Game>();
         UnleashSignal FrameTimer;
-        Stopwatch watch = new Stopwatch();
+        Stopwatch FrameRateReportWatch = new Stopwatch();
+        Stopwatch FrameDistanceWatch = new Stopwatch();
         public int GamesCount { get { return Games.Count; } set { } }
+        public uint ReportFrameRateIntervalInSeconds = 30;
+        public uint FramesExecuted = 0;
         bool StopExecutor = false;
 
         public GamesExecutor(int ID, UnleashSignal frameTimer)
@@ -24,27 +30,23 @@ namespace PingPongServer.GameExecution
 
         public void Run()
         {
-            watch.Start();
-            while(!StopExecutor)
+            FrameRateReportWatch.Start();
+            FrameDistanceWatch.Start();
+            while (!StopExecutor)
             {
                 FrameTimer.Lock();
-                watch.Stop();
-                int elapsedTime = (int)watch.ElapsedMilliseconds;
-                watch.Reset();
-                watch.Start();
+                FrameDistanceWatch.Stop();
                 foreach(Game game in Games.Entries)
                 {
-                    if (game.GameState == GameLogicLibrary.GameStates.Finished)
+                    if (game.GameState == GameStates.Finished)
                         Games.Remove(game);
                     else
-                        game.CalculateNextFrame(elapsedTime);
+                        game.CalculateNextFrame(((FrameDistanceWatch.ElapsedTicks*10000)/Stopwatch.Frequency));
                 }
+                FramesExecuted++;
+                FrameDistanceWatch.Restart();
+                ReportFrameRate();
             }
-        }
-
-        public void Stop()
-        {
-            StopExecutor = true;
         }
 
         public bool AddGame(Game game)
@@ -52,13 +54,89 @@ namespace PingPongServer.GameExecution
             if (StopExecutor)
             {
                 return false;
-            }   
+            }
             else
             {
                 Games.Add(game);
                 return true;
             }
-                
+
+        }
+
+        public bool AddObserversToGame(NetworkConnection observerConnection)
+        {
+            bool success = false;
+            foreach (Game game in Games.Entries)
+            {
+                if (game.AddObserver(observerConnection))
+                {
+                    success = true;
+                    break;
+                }
+            }
+            return success;
+        }
+
+        private void ReportFrameRate()
+        {
+            if (FrameRateReportWatch.ElapsedMilliseconds >= ReportFrameRateIntervalInSeconds * 1000)
+            {
+                uint FPS = FramesExecuted / ReportFrameRateIntervalInSeconds;
+                Logger.GamesExecutorLog("Frame Rate: " + FPS.ToString() + " FPS");
+                FramesExecuted = 0;
+                FrameRateReportWatch.Restart();
+            }   
+        }
+
+        private void RemoveFinishedGames()
+        {
+            foreach (Game game in Games.Entries)
+            {
+                if (game.GameState == GameStates.Finished)
+                {
+                    Logger.GamesManagerLog("Found a finished Game removing it now\n " + game.ToString());;
+                    Games.Remove(game);
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            StopAllGames();
+            StopExecutor = true;
+        }
+
+        private void StopAllGames()
+        {
+            foreach(Game game in Games.Entries)
+            {
+                game.StopGame();
+                Games.Remove(game);
+            }
+        }
+
+        public bool RejoinClientToGame(NetworkConnection clientWantsRejoin)
+        {
+            foreach (Game game in Games.Entries)
+            {
+                if(game.RejoinClient(clientWantsRejoin))
+                {
+                    Logger.GamesExecutorLog("Could successfully rejoin the client: " + clientWantsRejoin.RemoteEndPoint.ToString() 
+                        + " with Session ID: " + clientWantsRejoin.ClientSession.SessionID + "to the Game with ID: " + game.GameID.ToString());
+                    return true;
+                }
+                    
+            }
+            return false;
+        }
+
+        public int PlayersCurrentlyInGames()
+        {
+            int playersCurrentlyInGame = 0;
+            foreach (Game game in Games.Entries)
+                playersCurrentlyInGame += game.NumberOfPlayers;
+            return playersCurrentlyInGame;
+
         }
     }
 }
